09b143e39acd087b0b4684570619d182497592c1
OHOHA3 <lentevdanil123@gmail.com>
2024-05-01 16:30:34 +0700

    Initial

---
+++ SportInformationSystem/src/main/java/ru/nsu/sportinformationsystem/SportInformationSystemApplication.java
@@ -0,0 +1,11 @@
+package ru.nsu.sportinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportInformationSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SportInformationSystemApplication.class, args);
+    }
+}
